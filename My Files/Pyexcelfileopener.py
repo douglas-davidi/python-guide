3878a04dfe33bf34c73b2c94cd365eaf581cd296
douglas-davidi <59583281+douglas-davidi@users.noreply.github.com>
2020-04-17 11:18:19 -0400

    files
    new files

--- My Files/Pyexcelfileopener.py
+++ My Files/Pyexcelfileopener.py
@@ -26,3 +26,9 @@ for value in sheet.iter_rows(min_row=2,
                              max_col=7,
                              values_only=True):
     print(value)
+
+for value in sheet.iter_rows(min_row=2,
+                             min_col=4,
+                             max_col=8,
+                             values_only=True):
+    print(value)
